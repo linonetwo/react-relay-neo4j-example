891c060aea1aaa44db8fddb2a69945ee43f7ba2e
lineves <lindongwu11@gmail.com>
2015-12-05 23:03:44 +0800

    fix empty array bug

--- js/components/RelationshipPanel.js
+++ js/components/RelationshipPanel.js
@@ -13,30 +13,33 @@ class RelationshipPanel extends React.Component {
 	render() {
 	var {relationshipArray} = this.props.relationshipFromRelay;
 	var groupedList = []; // 把同一个植物拥有的特征都放到一起
-	let tempPlant = relationshipArray[0].plant  // 第一个植物
-	let tempFeatureArray = [];
-	relationshipArray.map(
-		item => {
-			// relationshipArray 是按照 plant.text 排序过的，所以只要现在考察的植物还是同一个，就把 feature 放到 tempFeatureArray 里
-			if(item.plant.text == tempPlant.text) {
-				tempFeatureArray.push( item.feature );
-			} else {
-				// 当考察到下一个植物的时候，就把上个植物的所有特征保存起来，并开始考察下一个植物
-				groupedList.push({
-					plant: tempPlant,
-					id: item.plant.id,
-					featureArray: tempFeatureArray,
-				})
-				tempPlant = item.plant;
-				tempFeatureArray = [];
+
+	if(relationshipArray.length > 0) {
+		let tempPlant = relationshipArray[0].plant  // 第一个植物
+		let tempFeatureArray = [];
+		relationshipArray.map(
+			item => {
+				// relationshipArray 是按照 plant.text 排序过的，所以只要现在考察的植物还是同一个，就把 feature 放到 tempFeatureArray 里
+				if(item.plant.text == tempPlant.text) {
+					tempFeatureArray.push( item.feature );
+				} else {
+					// 当考察到下一个植物的时候，就把上个植物的所有特征保存起来，并开始考察下一个植物
+					groupedList.push({
+						plant: tempPlant,
+						id: item.plant.id,
+						featureArray: tempFeatureArray,
+					})
+					tempPlant = item.plant;
+					tempFeatureArray = [];
+				}
 			}
-		}
-	)
-	groupedList.push({
-		plant: tempPlant,
-		id: relationshipArray.pop().plant.id,
-		featureArray: tempFeatureArray,
-	})
+		)
+		groupedList.push({
+			plant: tempPlant,
+			id: relationshipArray.pop().plant.id,
+			featureArray: tempFeatureArray,
+		})
+	}
 
 	
 	return (
@@ -46,23 +49,25 @@ class RelationshipPanel extends React.Component {
 			<CardText>
 			<List subheader="Relationship">
 				{
-					
-					groupedList.map(
-						item => {
-							return <ListItem
-								leftAvatar={<Avatar style={{'color':'#689F38', 'backgroundColor': '#C5E1A5'}}>{item.plant.text.substring(0,1)}</Avatar>}
-								primaryText={item.plant.text}
-								secondaryText={
-									<p>
-										{item.featureArray.map(
-											singleFeature => singleFeature.text+' '
-										)}
-									</p>
-								}
-								key={item.plant.id}
-								/>
-						}
-					)
+					() => {
+						if(relationshipArray.length > 0)
+						groupedList.map(
+							item => {
+								return <ListItem
+									leftAvatar={<Avatar style={{'color':'#689F38', 'backgroundColor': '#C5E1A5'}}>{item.plant.text.substring(0,1)}</Avatar>}
+									primaryText={item.plant.text}
+									secondaryText={
+										<p>
+											{item.featureArray.map(
+												singleFeature => singleFeature.text+' '
+											)}
+										</p>
+									}
+									key={item.plant.id}
+									/>
+							}
+						)
+					}
 				}
 			</List>
 			</CardText>
